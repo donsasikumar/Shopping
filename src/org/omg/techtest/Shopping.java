f86cbf00233483cc83ad17b6bb52e19226d11c9f
don <donsasikumar@outlook.com>
2016-10-21 00:21:10 +0100

    implemented step2

--- src/org/omg/techtest/Shopping.java
+++ src/org/omg/techtest/Shopping.java
@@ -8,7 +8,7 @@ import java.util.logging.Logger;
  * Created by don on 20/10/2016.
  */
 public class Shopping {
-    private enum Item {
+    enum Item {
         APPLE(0.60), ORANGE(0.25);
 
         Item(double price) {
@@ -19,7 +19,14 @@ public class Shopping {
     }
 
     private final List<Item> Basket = new ArrayList<>();
+    private volatile double total = 0;
     private final Logger LOGGER = Logger.getLogger("Shopping.class");
+
+
+    public double getTotal() {
+        return total;
+    }
+
 
     /**
      * getCart
@@ -30,27 +37,73 @@ public class Shopping {
     }
 
     /**
-     * addItemTo
+     * addItem
      * @param item which can be an Apple or an Orange
      * @return True if the Item is valid and was added to the list
      */
 
-    boolean addItemTo(Item item) {
+    boolean addItem(Item item) {
         return Basket.add(item);
     }
 
     /**
-     * getTotal
+     * calculateTotal
      * @return the total amount of all the items added to the cart.
      */
 
-    double getTotal() {
-        double total = 0;
+    double calculateTotal() {
+        total = 0;
         for (Item item : Basket) {
             total = total + item.price;
         }
         LOGGER.info(Basket + " => £" + total);
         return total;
+    }
+
+    /**
+     * getQuantity method
+     * @param item
+     * @return total Number of Item of that type added to the cart
+     */
+
+    int getQuantity(Item item){
+        int quantity = 0;
+        for(Item fruit:Basket){
+            if(fruit==item){
+                quantity++;
+            }
+        }
+        return quantity;
+    }
+
+    /**
+     * applyAppleOffer method
+     * @return total after applying apple offer
+     */
+    double applyAppleOffer(){
+        if(total==0){
+            total = calculateTotal();
+        }
+        total = total-(Item.APPLE.price*((getQuantity(Item.APPLE))/2));
+        System.out.printf("New Total after applying Apple Offer=> £%.2f", total);
+        System.out.println();
+        return total;
+    }
+
+    /**
+     * applyOrangeOffer
+     * @return total after applying orange offer
+     */
+
+    double applyOrangeOffer(){
+        if(total==0){
+            total = calculateTotal();
+        }
+        total = total - (Item.ORANGE.price*(getQuantity(Item.ORANGE)/3));
+        System.out.printf("New Total after applying Orange Offer=> £%.2f", total);
+        System.out.println();
+        return total;
+
     }
 
 }
--- src/org/omg/techtest/ShoppingTest.java
+++ src/org/omg/techtest/ShoppingTest.java
@@ -29,23 +29,82 @@ public class ShoppingTest {
     @Test
     public void test_AddItems(){
         Shopping instance = new Shopping();
-        Assert.assertTrue(instance.addItemTo(Shopping.Item.APPLE));
-        Assert.assertTrue(instance.addItemTo(Shopping.Item.ORANGE));
+        Assert.assertTrue(instance.addItem(Shopping.Item.APPLE));
+        Assert.assertTrue(instance.addItem(Shopping.Item.ORANGE));
         Assert.assertTrue(instance.getCart().size()==2);
-        Assert.assertTrue(instance.addItemTo(Shopping.Item.APPLE));
-        Assert.assertTrue(instance.addItemTo(Shopping.Item.ORANGE));
+        Assert.assertTrue(instance.addItem(Shopping.Item.APPLE));
+        Assert.assertTrue(instance.addItem(Shopping.Item.ORANGE));
         Assert.assertTrue(instance.getCart().size()==4);
     }
 
     @Test
     public void test_getTotal(){
         Shopping instance = new Shopping();
-        instance.addItemTo(Shopping.Item.APPLE);
-        instance.addItemTo(Shopping.Item.ORANGE);
-        instance.addItemTo(Shopping.Item.APPLE);
-        instance.addItemTo(Shopping.Item.APPLE);
-        Assert.assertTrue(instance.getTotal()==2.05);
+        instance.addItem(Shopping.Item.APPLE);
+        instance.addItem(Shopping.Item.ORANGE);
+        instance.addItem(Shopping.Item.APPLE);
+        instance.addItem(Shopping.Item.APPLE);
+        Assert.assertTrue(instance.calculateTotal()==2.05);
         }
+
+        @Test
+        public void test_GetQuantity(){
+            Shopping instance = new Shopping();
+            Assert.assertTrue(instance.getQuantity(Shopping.Item.APPLE)==0);
+            Assert.assertTrue(instance.getQuantity(Shopping.Item.ORANGE)==0);
+            Assert.assertTrue(instance.addItem(Shopping.Item.APPLE));
+            Assert.assertTrue(instance.addItem(Shopping.Item.ORANGE));
+            Assert.assertTrue(instance.getQuantity(Shopping.Item.APPLE)==1);
+            Assert.assertTrue(instance.getQuantity(Shopping.Item.ORANGE)==1);
+            Assert.assertTrue(instance.addItem(Shopping.Item.APPLE));
+            Assert.assertTrue(instance.getQuantity(Shopping.Item.APPLE)==2);
+            Assert.assertTrue(instance.getQuantity(Shopping.Item.ORANGE)==1);
+        }
+
+        @Test
+        public void test_AppleOffer(){
+            Shopping instance = new Shopping();
+            instance.addItem(Shopping.Item.APPLE);
+            instance.addItem(Shopping.Item.ORANGE);
+            instance.addItem(Shopping.Item.APPLE);
+            Assert.assertTrue(instance.calculateTotal()==1.45);
+            Assert.assertTrue(instance.applyAppleOffer()==0.85);
+        }
+
+    @Test
+    public void test_OrangeOffer1(){
+        Shopping instance = new Shopping();
+        instance.addItem(Shopping.Item.ORANGE);
+        instance.addItem(Shopping.Item.ORANGE);
+        instance.addItem(Shopping.Item.ORANGE);
+        Assert.assertTrue(instance.calculateTotal()==0.75);
+        Assert.assertTrue(instance.applyOrangeOffer()==0.50);
+    }
+
+    @Test
+    public void test_OrangeOffer2(){
+        Shopping instance = new Shopping();
+        instance.addItem(Shopping.Item.ORANGE);
+        instance.addItem(Shopping.Item.ORANGE);
+        instance.addItem(Shopping.Item.ORANGE);
+        instance.addItem(Shopping.Item.ORANGE);
+        instance.addItem(Shopping.Item.ORANGE);
+        instance.addItem(Shopping.Item.ORANGE);
+        Assert.assertTrue(instance.applyOrangeOffer()==1.0);
+    }
+
+    @Test
+    public void test_AppleOrangeOffers(){
+        Shopping instance = new Shopping();
+        instance.addItem(Shopping.Item.APPLE);
+        instance.addItem(Shopping.Item.ORANGE);
+        instance.addItem(Shopping.Item.APPLE);
+        instance.addItem(Shopping.Item.APPLE);
+        instance.calculateTotal();
+        instance.applyAppleOffer();
+        instance.applyOrangeOffer();
+        Assert.assertTrue(instance.getTotal()<1.46);
+    }
 
     }
 
